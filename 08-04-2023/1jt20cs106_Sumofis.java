5bf327b043d8d37f65ea81a61d1aff535e9a5807
Sai kiran J <saikiranj2002@gmail.com>
2023-04-08 23:24:11 +0530

    Create 1jt20cs106_Sumofis.java

---
+++ 08-04-2023/1jt20cs106_Sumofis.java
@@ -0,0 +1,37 @@
+package Addition;
+
+import java.util.Scanner;
+
+public class Sum {
+
+	public static void main(String[]args) {
+
+	int n,a,b;
+
+float sum;
+
+{
+
+	Scanner Sobj=new Scanner(System.in);
+
+	System.out.print("Enter the value of a");
+
+	a=Sobj.nextInt();
+
+	System.out.print("Enter the value of b");
+
+	b=Sobj.nextInt();
+
+	{
+
+		sum=a+b;
+
+	}
+
+System.out.print("sum of two number is:"+sum);
+
+}
+
+}
+
+} 
